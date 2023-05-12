02abacef403724b208e2b668c3644b11d834a0c6
tduong06 <133388650+tduong06@users.noreply.github.com>
2023-05-12 16:22:38 +0000

    Pending changes exported from your codespace

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text1: "ali hanh nghe rõ trả lời",
+  text2: "Tớ có điều này muốn hỏi hanh nhớ phải trả lời thật lòng nhaaa.",
+  text3: "hanh nhớ tớ phải không nào",
+  text4: "Nếu hanh ko trả lời mà thoát ra tức là muốn làm vợ tớ nha",
+  text5: "tduong mơ à!?",
+  text6: "nhớ vcl í",
+  text7: "Sao hanh lại nhớ tớ nhỉ?",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: "Hong bic nhma mún gặp tduong quáa",
+  text10: "Tớ biết mà, tớ nhớ hanh lắm,
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Tối nay chúng mình đi chơi nhaa. Còn giờ thì chờ gì nữa mà ko inb tớ nàooo",
+  text12: "aukay hehe",
 };
 
 $(document).ready(function () {
